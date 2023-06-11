4882b1b7cebf6c4e99d2f2c114cdb1adf999d5b1
Diana Huete <dianaht16@gmail.com>
2023-06-10 18:36:48 -0600

    first commit

---
+++ assets/js/components/hs.range-datepicker.js
@@ -0,0 +1,95 @@
+/**
+ * Range datepicker wrapper.
+ *
+ * @author Htmlstream
+ * @version 1.0
+ *
+ */
+;(function ($) {
+  'use strict';
+
+  $.HSCore.components.HSRangeDatepicker = {
+    /**
+     *
+     *
+     * @var Object _baseConfig
+     */
+    _baseConfig: {},
+
+    /**
+     *
+     *
+     * @var jQuery pageCollection
+     */
+    pageCollection: $(),
+
+    /**
+     * Initialization of Range datepicker wrapper.
+     *
+     * @param String selector (optional)
+     * @param Object config (optional)
+     *
+     * @return jQuery pageCollection - collection of initialized items.
+     */
+
+    init: function (selector, config) {
+
+      this.collection = selector && $(selector).length ? $(selector) : $();
+      if (!$(selector).length) return;
+
+      this.config = config && $.isPlainObject(config) ?
+        $.extend({}, this._baseConfig, config) : this._baseConfig;
+
+      this.config.itemSelector = selector;
+
+      this.initRangeDatepicker();
+
+      return this.pageCollection;
+
+    },
+
+    initRangeDatepicker: function () {
+      //Variables
+      var $self = this,
+        collection = $self.pageCollection;
+
+      //Actions
+      this.collection.each(function (i, el) {
+        //Variables
+        var $this = $(el),
+          optWrapper = $this.data('rp-wrapper'),
+          optIsInline = Boolean($this.data('rp-is-inline')),
+          optType = $this.data('rp-type'),
+          optMonths = $this.data('rp-months'),
+          optDateFormat = $this.data('rp-date-format'),
+          optDefaultDate = JSON.parse(el.getAttribute('data-rp-default-date')),
+          optIsDisableFutureDates = $this.data('rp-is-disable-future-dates');
+
+        $this.flatpickr({
+          inline: optIsInline, // boolean
+          mode: optType ? optType : 'single', // 'single', 'multiple', 'range'
+          dateFormat: optDateFormat ? optDateFormat : 'd M Y',
+          defaultDate: optDefaultDate,
+          appendTo: $(optWrapper)[0],
+          numberOfMonths: 2,
+          showMonths: optMonths ? optMonths : 1, 
+          maxDate: optIsDisableFutureDates ? 'hoy' : false,
+          locale: {
+            firstDayOfWeek: 1,
+            weekdays: {
+              shorthand: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"]
+            },
+            rangeSeparator: ' - '
+          }
+        });
+
+        $this.css({
+          width: $this.val().length * 7.5
+        });
+
+        //Actions
+        collection = collection.add($this);
+      });
+    }
+  };
+})(jQuery);
